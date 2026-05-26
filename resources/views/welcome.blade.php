88ed68c79c87c5ebf018d24c65ac6125651763ab
gmirilla <gmirilla@gmail.com>
2026-05-26 12:40:26 +0100

    color changes for kachia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -41,7 +41,7 @@
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
             <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
-                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none" style="background-color: #195519">
+                <div class="text-[13px] leading-[20px] flex-1 p-6 pb-12 lg:p-20 shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d] rounded-bl-lg rounded-br-lg lg:rounded-tl-lg lg:rounded-br-none" style="background-color: #FFCA28">
                     <h1 class="mb-1 font-medium" style="color: white">Welcome to Bandrspice Farm Monitoring and Evaluation Application</h1>
                     <p class="mb-2 text-[#706f6c] dark:text-[#A1A09A]">Bandrspice has an incredibly rich ecosystem. <br>We suggest starting with the following.</p>
                    
