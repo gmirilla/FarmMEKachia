88a5f3bf837f1973a96584250a6aaf2696370d3e
gmirilla <gmirilla@gmail.com>
2026-06-17 17:15:44 +0100

    add null coalescing for farm and report selection options

--- resources/views/inspection/inspection_new.blade.php
+++ resources/views/inspection/inspection_new.blade.php
@@ -5,14 +5,14 @@
                 @csrf
                 <select name="farmid" id="farmselect" class="form-select">
                     @forelse ( $farms as $farm )
-                        <option value="{{$farm->id}}">{{$farm->farmcode}}| {{$farm->farmname}}</option>  
+                        <option value="{{$farm->id ?? ' '}}">{{$farm->farmcode ?? 'N/A'}}| {{$farm->farmname ?? 'N/A'}}</option>  
                     @empty
                     <option value="0">No Farms have been assigned to you</option>
                     @endforelse
                 </select>
                 <select name="reportid" id="reportselect" class="form-select">
                     @forelse ( $reports as $report )
-                        <option value="{{$report->id}}">{{$report->reportname}}</option>  
+                        <option value="{{$report->id ?? ' '}}">{{$report->reportname ?? 'N/A'}}</option>  
                     @empty
                     <option value="0">No Reports configured on the system</option>
                     @endforelse
